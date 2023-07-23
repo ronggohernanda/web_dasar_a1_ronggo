b311f9e88ccd9dcf4231499b05693e360da89553
unknown <ronggohernanda23@gmail.com>
2023-07-23 14:13:33 +0700

    push github saya

--- pertemuan5/index.php
+++ pertemuan5/index.php
@@ -4,37 +4,43 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Data Produk</title>
+    <title>Link</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-
-    <h1>Data Produk</h1>
-    <?php
-        $produk = array("Samsung","Acer","ROG");
-        $jumlah_produk = count($produk);
-    ?>
-    <table border="1" style= "border-collapse">
-    <tr>
-        <th>No</th>
-        <th>Nama Produk</th>
-    </tr>
-    <?php
-    for ($i=0; $i <$jumlah_produk; $i++) { ?>
-    <tr>
-        <td><?=$i+1; ?></td>
-        <td><?=$produk [$i]; ?></td>
-    </tr>
-    <?php } ?>
+    <h1>Data Mahasiswa</h1>
+    <a href="tambah_mahasiswa.php">Tambah Mahasiswa</a>
+    <table border="1">
+        <tr>
+            <th>No</th>
+            <th>NPM</th>
+            <th>Nama Mahasiswa</th>
+            <th>Jenis Kelamin</th>
+            <th>Tanggal Lahir</th>
+            <th>Alamat</th>
+            <th>Program Studi</th>
+            <th>Opsi</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>98765</td>
+            <td>Nugik</td>
+            <td>Cewek</td>
+            <td>01/01/2001</td>
+            <td>Siborong-borong</td>
+            <td>Sistem Informasi</td>
+            <td><a href="edit_mahasiswa.php">Edit</a></td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>98766</td>
+            <td>Nugito</td>
+            <td>Pria</td>
+            <td>01/01/2002</td>
+            <td>Siborong-borong</td>
+            <td>Teknik Informasi</td>
+            <td><a href="edit_mahasiswa.php">Edit</a></td>
+        </tr>
     </table>
-    <hr>
-    <ul>
-        <li>
-            <a href="index.php">Indexed Arrays</a></li>
-        <li>
-            <a href="aa.php">Associative Arrays</a></li>
-        <li>
-            <a href="ma.php">Multidimensional Arrays</a> </li>
-
-    </ul>
 </body>
 </html>
