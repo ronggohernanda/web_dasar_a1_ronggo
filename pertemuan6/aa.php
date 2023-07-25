8ac35a98dcb0aca8ae23b14e113b1ea0c6f3986e
unknown <ronggohernanda23@gmail.com>
2023-07-25 17:35:53 +0700

    membuat tabel mahasiswa dan edit mahasiswa

--- pertemuan6/aa.php
+++ pertemuan6/aa.php
@@ -1,28 +1,45 @@
-<?php
-$mobil = array("Civic","Avanza","Supra");
-?>
-
-<table border="1" style="border-collapse: collapse">
-    <tr>
-        <th>No</th>
-        <th>Nama Mobil</th>
-    </tr>
-    <tr>
-        <td>1</td>
-        <td><?php echo $mobil[0] ?></td>
-    </tr>
-    <tr>
-        <td>1</td>
-        <td><?php echo $mobil[1] ?></td>
-    </tr>
-    <tr>
-        <td>1</td>
-        <td><?php echo $mobil[2] ?></td>
-    </tr>
-</table>
-
-<ul>
-    <li><?= $mobil[0] ?></li>
-    <li><?= $mobil[1] ?></li>
-    <li><?= $mobil[2] ?></li>
-</ul>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Associative Arrays</title>
+</head>
+<body>
+    <h1>Associative Arrays</h1>
+    <ul>
+        <li>
+            <a href="index.php">Indexed Arrays</a>
+        </li>
+        <li>
+            <a href="aa.php">Associative Arrays</a>
+        </li>
+        <li>
+            <a href="ma.php">Multidimensional Arrays</a>
+        </li>
+    </ul>
+    <hr>
+    <?php
+    $umur = array("Leo" => "20","Juli" => "17","Habibi" => "25");
+    // var_dump($umur);
+    // echo $umur["Leo"];
+    ?>
+    <table border="1" style="border-collapse: collapse">
+        <tr>
+            <th>No</th>
+            <th>Nama</th>
+            <th>Umur</th>
+        </tr>
+        <?php
+        $number = 1;
+        foreach($umur as $u => $u_value) : ?>
+        <tr>
+            <td><?= $number++ ?></td>
+            <td><?= $u; ?></td>
+            <td><?= $u_value; ?></td>
+        </tr>
+        <?php endforeach; ?>
+    </table>
+</body>
+</html>
